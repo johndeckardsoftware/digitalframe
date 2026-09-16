f8f9ac5bdfd73e67d3aa095eabeb120f7e00c501
johndeckardsoftware <77699416+johndeckardsoftware@users.noreply.github.com>
2026-09-16 19:36:23 +0200

    Update rpi_bluedot.py

--- src/rpi_bluedot.py
+++ src/rpi_bluedot.py
@@ -6,7 +6,6 @@ logger = logging.getLogger(__name__)
 
 class BlueDotRemote():
     def __init__(self, controller, devices):
-        logger.setLevel(Config.get("window.log_level", logging.INFO))
         #logger.setLevel(logging.DEBUG)
         self.df = controller
         self.devices = devices
